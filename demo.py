c06e444de0f96cd6f9ee81423bc74cae32812e16
Theran <theran.brigowatz@gmail.com>
2020-01-01 11:25:35 +0900

    picking up the basics

---
+++ demo.py
@@ -0,0 +1,26 @@
+# Hello World
+
+print("Hello World!")
+print("Goodbye World!")
+print("Goodbye World!")
+print(44 + 1)
+
+# Numbers and Math
+print(10/5.0)
+
+# Variables
+variable_name = 5
+print(variable_name + variable_name)
+print(variable_name * variable_name)
+
+# Define a variable named city and set it equal to any string
+city = "Seoul"
+# Define a variable named price and set it equal to any float
+price = 3.0
+# Define a variable named high_score and set it equal to any int
+high_score = 200000
+# Define a variable named is_having_fun and set it to a Boolean value
+is_having_fun = True
+
+new_line = "hi \nthere"
+print(new_line)
