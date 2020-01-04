a5f3dbcfd169ccfa6058a2f6bc456af118b544f6
Theran <theran.brigowatz@gmail.com>
2020-01-04 19:26:17 +0900

    more dictionaries

--- demo_notes.py
+++ demo_notes.py
@@ -1,5 +1,6 @@
 # Hello World
 
+from random import choice  # DON'T CHANGE!
 from random import choice, randint
 from random import randint  # |  \
 from random import randint
@@ -292,3 +293,75 @@ artist = {
 }
 
 full_name = artist['first'] + " " + artist['last']
+
+# DON'T TOUCH PLEASE!
+donations = dict(sam=25.0, lena=88.99, chuck=13.0, linus=99.5,
+                 stan=150.0, lisa=50.25, harrison=10.0)
+# DON'T TOUCH PLEASE!
+
+
+# Use a loop to add together all the donations and store the resulting number in a variable called total_donations
+total_donations = 0
+for donation in donations.values():
+    total_donations += donation
+
+# This code picks a random food item:
+food = choice(["cheese pizza", "quiche", "morning bun",
+               "gummy bear", "tea cake"])  # DON'T CHANGE!
+
+# DON'T CHANGE THIS DICTIONARY EITHER!
+bakery_stock = {
+    "almond croissant": 12,
+    "toffee cookie": 3,
+    "morning bun": 1,
+    "chocolate chunk cookie": 9,
+    "tea cake": 25
+}
+
+# YOUR CODE GOES BELOW:
+quantity = bakery_stock.get(food)
+if quantity:
+    print("{quantity} left".format())
+else:
+    print("We don't make that")
+
+# DO NOT TOUCH game_properties!
+game_properties = ["current_score", "high_score", "number_of_lives", "items_in_inventory", "power_ups", "ammo",
+                   "enemies_on_screen", "enemy_kills", "enemy_kill_streaks", "minutes_played", "notifications", "achievements"]
+
+# Use the game_properties list and dict.fromkeys() to generate a dictionary with all values set to 0. Save the result to a variable called initial_game_state
+initial_game_state = dict.fromkeys(game_properties, 0)
+
+
+inventory = {'croissant': 19, 'bagel': 4,
+             'muffin': 8, 'cake': 1}  # DON'T CHANGE THIS LINE!
+
+# Make a copy of inventory and save it to a variable called stock_list USE A DICTIONARY METHOD
+stock_list = inventory.copy()
+
+# add the value 18 to stock_list under the key "cookie"
+
+stock_list["cookie"] = 18
+
+
+# remove 'cake' from stock_list USE A DICTIONARY METHOD
+stock_list.pop("cake")
+
+list1 = ["CA", "NJ", "RI"]
+list2 = ["California", "New Jersey", "Rhode Island"]
+
+# make sure your solution is assigned to the answer variable so the tests can work!
+answer = {list1[i]: list2[i] for i in range(0, 3)}
+
+person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
+
+# use the person variable in your answer
+answer = {item[0]: item[1] for item in person}
+
+# make sure your solution is assigned to the answer variable so the tests can work!
+letters = 'aeiou'
+answer = {letter: 0 for letter in l"aeiou"}
+
+# ASCII Dictionary
+# make sure your solution is assigned to the answer variable so the tests can work!
+answer = {letter: chr(letter) for letter in range(65, 91)}
