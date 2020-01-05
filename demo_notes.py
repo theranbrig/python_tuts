2a9aea0a7aa61f36e8e8e8c10922402f5d1ccd96
Theran <theran.brigowatz@gmail.com>
2020-01-05 19:04:00 +0900

    functions

--- demo_notes.py
+++ demo_notes.py
@@ -1,5 +1,6 @@
 # Hello World
 
+from random import random
 from random import choice  # DON'T CHANGE!
 from random import choice, randint
 from random import randint  # |  \
@@ -306,62 +307,188 @@ for donation in donations.values():
     total_donations += donation
 
 # This code picks a random food item:
-food = choice(["cheese pizza", "quiche", "morning bun",
-               "gummy bear", "tea cake"])  # DON'T CHANGE!
+# food = choice(["cheese pizza", "quiche", "morning bun",
+#                "gummy bear", "tea cake"])  # DON'T CHANGE!
 
-# DON'T CHANGE THIS DICTIONARY EITHER!
-bakery_stock = {
-    "almond croissant": 12,
-    "toffee cookie": 3,
-    "morning bun": 1,
-    "chocolate chunk cookie": 9,
-    "tea cake": 25
+# # DON'T CHANGE THIS DICTIONARY EITHER!
+# bakery_stock = {
+#     "almond croissant": 12,
+#     "toffee cookie": 3,
+#     "morning bun": 1,
+#     "chocolate chunk cookie": 9,
+#     "tea cake": 25
+# }
+
+# # YOUR CODE GOES BELOW:
+# quantity = bakery_stock.get(food)
+# if quantity:
+#     print("{quantity} left".format())
+# else:
+#     print("We don't make that")
+
+# # DO NOT TOUCH game_properties!
+# game_properties = ["current_score", "high_score", "number_of_lives", "items_in_inventory", "power_ups", "ammo",
+#                    "enemies_on_screen", "enemy_kills", "enemy_kill_streaks", "minutes_played", "notifications", "achievements"]
+
+# # Use the game_properties list and dict.fromkeys() to generate a dictionary with all values set to 0. Save the result to a variable called initial_game_state
+# initial_game_state = dict.fromkeys(game_properties, 0)
+
+
+# inventory = {'croissant': 19, 'bagel': 4,
+#              'muffin': 8, 'cake': 1}  # DON'T CHANGE THIS LINE!
+
+# # Make a copy of inventory and save it to a variable called stock_list USE A DICTIONARY METHOD
+# stock_list = inventory.copy()
+
+# # add the value 18 to stock_list under the key "cookie"
+
+# stock_list["cookie"] = 18
+
+
+# # remove 'cake' from stock_list USE A DICTIONARY METHOD
+# stock_list.pop("cake")
+
+# list1 = ["CA", "NJ", "RI"]
+# list2 = ["California", "New Jersey", "Rhode Island"]
+
+# # make sure your solution is assigned to the answer variable so the tests can work!
+# answer = {list1[i]: list2[i] for i in range(0, 3)}
+
+# person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
+
+# # use the person variable in your answer
+# answer = {item[0]: item[1] for item in person}
+
+# # # make sure your solution is assigned to the answer variable so the tests can work!
+# # letters = 'aeiou'
+# # answer = {letter: 0 for letter in l"aeiou"}
+
+# # ASCII Dictionary
+# # make sure your solution is assigned to the answer variable so the tests can work!
+# answer = {letter: chr(letter) for letter in range(65, 91)}
+
+# # 1 - Create a variable called numbers which is a tuple with the the values 1, 2, 3 and 4 inside.
+# numbers = (1, 2, 3, 4)
+
+# # 2 - Create a variable called value which is a tuple with the the value 1 inside.
+# value = (1,)
+
+# # 3 - Given the following variable:
+
+# values = [10, 20, 30]
+
+# # Create a variable called static_values which is the result of the values variable converted to a tuple
+
+# static_values = tuple(values)
+
+# # 4 - Given the following variable
+
+# stuff = [1, 3, 1, 5, 2, 5, 1, 2, 5]
+
+# # Create a variable called unique_stuff which is a set of only the unique values in the stuff list
+
+# unique_stuff = set(stuff)
+
+
+def sing_happy_bday():
+    print("hbdty")
+    print("hbdty")
+    print("hbdtdp")
+    print("hbdty")
+
+
+sing_happy_bday()
+sing_happy_bday()
+
+# Define your make_noise function below
+
+
+def make_noise():
+    print("THE CROWD GOES WILD")
+# Then, call make_noise once:
+
+
+make_noise()
+
+
+def flip_coin():
+    if random() <= 0.5:
+        return "It was heads."
+    else:
+        return "It was tails"
+
+
+print(flip_coin())
+
+
+def speak_pig():
+    return 'oink'
+
+# Define a function called generate_evens
+# It should return a list of even numbers between 1 and 50(not including 50)
+
+
+def generate_evens():
+    return [num for num in range(1, 50) if num % 2 == 0]
+
+
+def yell(string):
+    return string.upper() + "!"
+
+# Without adding any new lines of code, make count_dollar_signs work as intended
+
+
+def count_dollar_signs(word):
+    count = 0
+    for char in word:
+        if char == '$':
+            count += 1
+    return count
+
+
+# Define speak below:
+noises = {
+    "dog": "woof",
+    "pig": "oink",
+    "duck": "quack",
+    "cat": "meow"
 }
 
-# YOUR CODE GOES BELOW:
-quantity = bakery_stock.get(food)
-if quantity:
-    print("{quantity} left".format())
-else:
-    print("We don't make that")
 
-# DO NOT TOUCH game_properties!
-game_properties = ["current_score", "high_score", "number_of_lives", "items_in_inventory", "power_ups", "ammo",
-                   "enemies_on_screen", "enemy_kills", "enemy_kill_streaks", "minutes_played", "notifications", "achievements"]
+def speak(animal="dog"):
+    noise = noises.get(animal)
+    if noise:
+        return noise
+    return "?"
 
-# Use the game_properties list and dict.fromkeys() to generate a dictionary with all values set to 0. Save the result to a variable called initial_game_state
-initial_game_state = dict.fromkeys(game_properties, 0)
+# define product below:
 
 
-inventory = {'croissant': 19, 'bagel': 4,
-             'muffin': 8, 'cake': 1}  # DON'T CHANGE THIS LINE!
-
-# Make a copy of inventory and save it to a variable called stock_list USE A DICTIONARY METHOD
-stock_list = inventory.copy()
-
-# add the value 18 to stock_list under the key "cookie"
-
-stock_list["cookie"] = 18
+def product(a, b):
+    return a * b
 
 
-# remove 'cake' from stock_list USE A DICTIONARY METHOD
-stock_list.pop("cake")
+days = {1: "Sunday", 2: "Monday", 3: "Tuesday",
+        4: "Wednesday", 5: "Thursday", 6: "Friday", 7: "Saturday"}
 
-list1 = ["CA", "NJ", "RI"]
-list2 = ["California", "New Jersey", "Rhode Island"]
 
-# make sure your solution is assigned to the answer variable so the tests can work!
-answer = {list1[i]: list2[i] for i in range(0, 3)}
+def return_day(day):
+    is_day = days.get(day)
+    if is_day:
+        return is_day
+    else:
+        return None
 
-person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
 
-# use the person variable in your answer
-answer = {item[0]: item[1] for item in person}
+def last_element(arr):
+    if arr:
+        return arr[-1]
+    return None
 
-# make sure your solution is assigned to the answer variable so the tests can work!
-letters = 'aeiou'
-answer = {letter: 0 for letter in l"aeiou"}
 
-# ASCII Dictionary
-# make sure your solution is assigned to the answer variable so the tests can work!
-answer = {letter: chr(letter) for letter in range(65, 91)}
+def number_compare(a, b):
+    if a > b:
+        return "First is greater"
+    elif a < b:
+        return "Second is greater"
+    return "Numbers are equal"
