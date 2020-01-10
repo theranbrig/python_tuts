659ebf0434a12225b310f2b9c0488fda195cd382
Theran <theran.brigowatz@gmail.com>
2020-01-10 19:22:30 +0900

    deck of cards class

---
+++ deck_of_cards.py
@@ -0,0 +1,57 @@
+import random
+
+
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def __repr__(self):
+        return f"{self.value} {self.suit}"
+
+
+class Deck:
+    def __init__(self):
+        nums = ("A", "2", "3", "4", "5", "6", "7",
+                "8", "9", "10", "J", "Q", "K")
+        suits = ("♦", "♣", "♠", "♦")
+        new_deck = [Card(suit, num) for num in nums for suit in suits]
+        self.cards = new_deck
+
+    def __repr__(self):
+        cards_remaining = len(self.cards)
+        return f"Deck of {cards_remaining} cards"
+
+    def _deal(self, num):
+        count = 0
+        if num < len(self.cards):
+            num = len(self.cards)
+        if len(self.cards) == 0:
+            return ValueError("All cards have been dealt")
+        while count < num:
+            self.cards.pop()
+
+    def shuffle(self):
+        if len(self.cards) < 52:
+            return ValueError("Only full decks can be shuffled")
+        else:
+            return random.shuffle(self.cards)
+
+    def deal_card(self):
+        return self.cards.pop()
+
+    def deal_hand(self, num):
+        count = 0
+        hand = []
+        while count < num:
+            card = self.cards.pop()
+            hand.append(card)
+            count += 1
+        return hand
+
+
+my_deck = Deck()
+print(my_deck)
+print(my_deck.shuffle())
+print(my_deck.deal_card())
+print(my_deck.deal_hand(5))
