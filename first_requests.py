c385cdfb1a1fbf54f4ac663030cb856115b8336c
Theran <theran.brigowatz@gmail.com>
2020-01-10 10:28:19 +0900

    dad joke generator

--- first_requests.py
+++ first_requests.py
@@ -1,15 +1,26 @@
 import requests
+from termcolor import colored
+from pyfiglet import figlet_format as format
 
 # url = 'http://www.google.com'
 # res = requests.get(url)
 
 # print(f"Request: {url}, Status Code: {res.status_code}")
+print(colored(format("DAD JOKE 3000"), color="cyan"))
 
-url = "https://icanhazdadjoke.com/"
+search = input("What kind of joke would you like? ")
+url = "https://icanhazdadjoke.com/search"
 
-res = requests.get(url, headers={"Accept": "application/json"})
+res = requests.get(
+    url, headers={"Accept": "application/json"}, params={"term": search})
 
 data = res.json()
 
-print(res.text)
-print(data["joke"])
+joke_count = 0
+if len(data["results"]) == 0:
+    print("Sorry.  No jokes found")
+else:
+    for item in data["results"]:
+        joke_count += 1
+        print(f"JOKE {joke_count}")
+        print(item["joke"])
